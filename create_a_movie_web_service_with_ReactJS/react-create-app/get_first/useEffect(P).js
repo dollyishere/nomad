b83f9e7a1778bb97c22c62d7e800e9e95abbc5ee
dollyishere <rundollyrun8@gmail.com>
2023-01-17 17:58:05 +0900

    update

---
+++ create_a_movie_web_service_with_ReactJS/react-create-app/get_first/useEffect(P).js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  // 1. useEffect가 지켜볼 대상이 없을 시
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  // 2. 지켜볼 대상이 하나일 시
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  // 3. 지켜볼 대상이 하나 이상일 시
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
