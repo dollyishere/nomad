2be1e3f21ebb2db0ed3adfd2b1fd544c51ae1d94
dollyishere <rundollyrun8@gmail.com>
2023-01-19 11:10:37 +0900

    backup

--- create_a_movie_web_service_with_ReactJS/react-create-app/my-app/src/App.js
+++ create_a_movie_web_service_with_ReactJS/react-create-app/my-app/src/App.js
@@ -1,28 +1,20 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+import Movie from "./components/Movie"
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([])
-  const getMovies = async() => {
-    const re
-  }
-  useEffect(() => {
-    fetch(
-      ``
-    )
-    .then((response) => response.json())
-    .then((json) => {
-      setMovies(json.data.movies)
-      setLoading(false)
-    })
-  }, [])
-  return(
-    <div>
-      {loading ?
-      <h1>Loading...</h1>:
-      null
-      }
-    </div>
+  // 이 경우 Router(BrowserRouter(Hash와는 url 작성 시 # 유무의 차이))가 가장 먼저 렌더링됨
+  // Routes는 2종 이상의 라우팅을 가능하게 함(switch의 대체)
+  return (
+  <Router>
+  <Routes> 
+  <Route path="/hello" element={<h1>hello</h1>}/>
+  <Route path="/" element={<Home />} />
+  <Route path="/movie/:id" element={<Detail />} />
+  </Routes>
+  </Router>
   )
 }
 
